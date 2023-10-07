716eedede3878db46b89be5ebfd2a85f56cd474b
Valentina Beltran <valbeltran.ca@gmail.com>
2023-10-07 12:38:01 -0500

    este es mi primer commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require('colors');
+
+const { guardarDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu, 
+    pausa,
+    leerInput
+} = require('./helpers/inquirer');
+const Tareas = require('./models/tareas')
+
+
+const main = async() => {
+    console.log('Hola mundo');
+
+    let opt = '';
+    const tareas = new Tareas();
+
+     do {
+       opt = await inquirerMenu();
+       console.log({ opt });
+            
+       switch (opt){
+        case '1':
+            // crear opcion
+             const desc = await leerInput('Descripción: ');
+             tareas.crearTarea( desc )
+        break;
+
+        case '2':
+            console.log( tareas.listadoArr );
+        break; 
+ }
+
+
+ //guardarDB( tareas.listadoArr );
+
+
+        await pausa();
+     } while( opt !== '0');
+
+
+    //pausa();
+
+}
+
+
+main();
+
+
+
---
+++ models/tareas.js
@@ -0,0 +1,37 @@
+const Tarea = require ('./tarea');
+/**
+ * _listado:
+ * { 'uuid-123712-123123-2:{ id:12, desc:asd,completadoEN:92231 } },
+ */
+class Tareas {
+
+    _listado = {};
+
+    get listadoArr(){
+
+        const listado = [];
+        Object.keys(this._listado).forEach( key =>{
+            const tarea = this._listado[key];
+            listado.push( tarea );
+                });
+
+        return listado;
+    }
+
+    constructor() {
+        this._listado = {};
+    }
+
+    crearTarea( desc = '' ) {
+
+        const tarea = new Tarea(desc);
+
+        this._listado[tarea.id] = tarea;
+
+    }
+    
+}
+
+
+
+module.exports = Tareas;
